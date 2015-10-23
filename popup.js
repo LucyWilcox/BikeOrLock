816410842adaa2a710c93d501f96b1138f5e3b22
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-10-22 21:00:32 -0400

    Working on hitting json

--- popup.js
+++ popup.js
@@ -54,42 +54,70 @@
  * @param {function(string)} errorCallback - Called when the image is not found.
  *   The callback gets a string that describes the failure reason.
  */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
+// function getImageUrl(searchTerm, callback, errorCallback) {
+//   // Google image search - 100 searches per day.
+//   // https://developers.google.com/image-search/
+//   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
+//     '?v=1.0&q=' + encodeURIComponent(searchTerm);
+//   var x = new XMLHttpRequest();
+//   x.open('GET', searchUrl);
+//   // The Google image search API responds with JSON, so let Chrome parse it.
+//   x.responseType = 'json';
+//   x.onload = function() {
+//     // Parse and process the response from Google Image Search.
+//     var response = x.response;
+//     if (!response || !response.responseData || !response.responseData.results ||
+//         response.responseData.results.length === 0) {
+//       errorCallback('No response from Google Image search!');
+//       return;
+//     }
+//     var firstResult = response.responseData.results[0];
+//     // Take the thumbnail instead of the full image to get an approximately
+//     // consistent image size.
+//     var imageUrl = firstResult.tbUrl;
+//     var width = parseInt(firstResult.tbWidth);
+//     var height = parseInt(firstResult.tbHeight);
+//     console.assert(
+//         typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
+//         'Unexpected respose from the Google Image Search API!');
+//     callback(imageUrl, width, height);
+//   };
+//   x.onerror = function() {
+//     errorCallback('Network error.');
+//   };
+//   x.send();
+// }
+
+function getData(callback){
+    $.ajax({
+        url: 'https://peaceful-reef-6842.herokuapp.com/bikeSession',
+        dataType: 'json',
+        success: function(json) {
+            console.log(json);
+            distance = json.distance;
+            console.log(rates);
+            callback(distance);
+        }
+    });
 }
 
-function readTextFile(callback)
-{
+getData();
+
+// function getData2(callback){
+//   var peaceful = "https://peaceful-reef-6842.herokuapp.com/bikeSession";
+//   $.getJson(peaceful, function(data) {
+//     var items = [];
+//     $.each(data, function)
+//   }
+    
+//   })
+// }
+
+
+
+
+
+function readTextFile(callback){
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", "test.txt", false);
     rawFile.onload = function ()
