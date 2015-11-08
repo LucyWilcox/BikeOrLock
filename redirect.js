b5f4839715ae982567a03d704c4151f29837507d
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-11-08 17:23:01 -0500

    Whoops... on master

--- redirect.js
+++ redirect.js
@@ -1,4 +1,7 @@
+var pattern = new UrlPattern('(http(s)\\://)(:subdomain.):domain.:tld(/*)');
+
 chrome.extension.onRequest.addListener(function(request, sender) {
+  var domain = pattern.match(sender.url);
   // console.log(request);
   // console.log(sender);
   //   if (sender.url.match("http://www.netflix.com/browse")){
