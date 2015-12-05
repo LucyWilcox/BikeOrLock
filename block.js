0b84af9f714000b8ddfdcc8608a8804d6da1ad26
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-12-05 08:04:49 -0500

    Done!

--- block.js
+++ block.js
@@ -1,13 +1,10 @@
 var bikenchill = "http://bikechill.herokuapp.com/dashboard";
-var port = chrome.runtime.connect({name:"block"}); // magic to me, opens a port to get stuff sets in options
 
 function checkBlocking(domain){
   var blockedSites = localStorage.getItem("blockedSites");
   var blockedList = blockedSites.split(',');
-  console.log(blockedList);
   if (blockedList.indexOf(domain) > -1){
-    console.log("hi");
-    chrome.extension.sendRequest({end: bikenchill});
+    chrome.extension.sendRequest({redirectsite: bikenchill});
   }
 }
 
@@ -26,7 +23,6 @@ function getData(callback){ //hits webapp gets all json that is there
 getData(function(response){ // response is json object
   var blockedSites = response.users.blockedDomains;
   localStorage.setItem("blockedSites" , blockedSites);
-  // getURL();
 });
 
 function checkURL(url){
@@ -39,61 +35,5 @@ function checkURL(url){
 
 chrome.runtime.onMessage.addListener(
   function(message, sender){
-    console.log(message);
+    checkURL(message.url);
   });
-
-// port.onMessage.addListener(function(message,sender){
-//     url = message.url;
-
-//     console.log(url);
-//     chrome.runtime.onConnect.addListener(function(port){
-//       port.postMessage({"url" : null}); //send message into port as chosenDistance, used in content.js
-//     });
-//     // port.postMessage({"url" : null});
-
-//   });
-
-// function getURL(callback){
-//   port.onMessage.addListener(function(message,sender){
-//     url = message.url;
-//     // console.log(sender);
-//     // console.log(message);
-//     if(url != localStorage.getItem("url")){
-//       callback(url);
-//     }
-//     // localStorage.setItem("url", url);
-//     // console.log(url);
-//     // localStorage.setItem("url", url);
-//     // if (message.url){
-//     //   // checkURL(url);
-//     // }
-//     // checkURL(url);
-//   });
-// }
-
-// getURL(function(response){
-//   var url = response;
-//   localStorage.setItem("url", url);
-//   console.log(url);
-//   // checkURL(url);
-// });
-
-// getURL();
-// getURL();
-// function sleep(milliseconds) {
-//   var start = new Date().getTime();
-//   for (var i = 0; i < 1e7; i++) {
-//     if ((new Date().getTime() - start) > milliseconds){
-//       break;
-//     }
-//   }
-// }
-
-// var url = localStorage.getItem("url");
-//   for(var i = 0; i< 5; i++){
-//     getURL();
-//     var url = localStorage.getItem("url");
-//     console.log(url);
-//     sleep(1000);
-//   }
-
--- redirect.js
+++ redirect.js
@@ -1,5 +1,3 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {"url" : tab.url});  
-  });
-}); 
+chrome.extension.onRequest.addListener(function(request, sender) {
+  chrome.tabs.update(sender.tab.id, {url: request.redirectsite} );
+});
